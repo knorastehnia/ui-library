c33c3890761f94e6bf1cc2c85395c7fa23b2a557
Karan <k.norastehnia@gmail.com>
2025-08-21 22:47:00 -0400

    Update Card element

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -24,10 +24,20 @@ const Card = () => {
         className='border'
         ref={borderRef}
         style={{
-          maskImage: `radial-gradient(200px 200px at ${mousePos.x}px ${mousePos.y}px, #000 0%, #00000030)`
+          maskImage: `radial-gradient(150px 150px at ${mousePos.x}px ${mousePos.y}px, #000 0%, #00000020)`
         }}
       >
-        <div className='card'></div>
+        <div className='card'>
+          <svg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'>
+            <filter id='noise'>
+              <feTurbulence 
+                type='fractalNoise' 
+                baseFrequency='0.65' 
+                numOctaves='3' 
+                stitchTiles='stitch'/>
+            </filter>
+          </svg>
+        </div>
       </div>
     </>
   )
