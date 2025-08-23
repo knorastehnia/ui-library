5823a26c91868c7e631eb27295c54e69707eba31
Karan <k.norastehnia@gmail.com>
2025-08-23 10:26:16 -0400

    Dynamic border radius + Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Card from './components/core/Card'
 import Button from './components/core/Button'
+import Input from './components/core/Input'
 
 const App = () => {
   return (
@@ -25,8 +26,14 @@ const App = () => {
           </div>
         </Card>
 
-        <Card>
-          Content
+        <Card height={300}>
+          <Input type='text' name='input'>Input</Input>
+          <h1>H1 Tag</h1>
+          <h2>H2 Tag</h2>
+          <h3>H3 Tag</h3>
+          <h4>H4 Tag</h4>
+          <h5>H5 Tag</h5>
+          <h6>H6 Tag</h6>
         </Card>
       </div>
     </>
