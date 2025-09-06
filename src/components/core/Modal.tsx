0d5420cf56cc92580cfeea5640b6e23658909301
Karan <k.norastehnia@gmail.com>
2025-09-05 23:28:43 -0400

    Add Modal component

---
+++ src/components/core/Modal.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+import Card from './Card'
+import styles from './Modal.module.css'
+
+interface ModalProps {
+  children: React.ReactNode,
+  isOpen: boolean,
+  setIsOpen: Function,
+  width?: string,
+  height?: string,
+}
+
+const Modal: React.FC<ModalProps> = ({
+  children,
+  isOpen,
+  setIsOpen,
+  width='auto',
+  height='auto',
+}) => {
+  const escapeModal = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setIsOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', escapeModal)
+    return () => document.removeEventListener('keydown', escapeModal)
+  }, [])
+
+  return createPortal(
+    <div
+      className={`
+        ${styles['modal']} 
+        ${isOpen && styles['open']}
+      `}
+    >
+      <div
+        className={styles['modal-bg']}
+        onClick={() => setIsOpen(false)}
+      ></div>
+
+      <div className={styles['content']}>
+        <Card width={width} height={height}>{children}</Card>
+      </div>
+    </div>
+
+    , document.querySelector('#root')!
+  )
+}
+
+export default Modal
