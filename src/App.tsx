c0b14595d8914899316ec284d1e675f50ed3df3d
Karan <k.norastehnia@gmail.com>
2025-09-19 23:03:13 -0400

    Style changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,33 @@ const App = () => {
     <>
       <Layout>
         <Layout.Sidebar>
-          <Tree.Item label='Tree Item' href='#' />
-          <Tree.Item label='Tree Item' href='#' />
+          <Tree.Item label='Dashboard' href='#' />
+          <Tree.Item label='Projects' href='#' />
           <Tree.Item label='Tree Item' href='#' />
 
           <Tree>
-            <Tree.Branch label='Tree Branch'>
-              <Tree.Item label='Tree Item' href='#' />
-              <Tree.Item label='Tree Item' href='#' />
-              <Tree.Item label='Tree Item' href='#' />
-              <Tree.Item label='Tree Item' href='#' />
+            <Tree.Branch label='Assets'>
+              <Tree.Item label='Item #1' href='#' />
+              <Tree.Item label='Tree Item #2' href='#' />
+              <Tree.Item label='Tree Item #3' href='#' />
+              <Tree.Item label='Item #4' href='#' />
             </Tree.Branch>
 
-            <Tree.Branch label='Tree Branch'>
-              <Tree.Branch label='Tree Branch'>
+            <Tree.Branch label='Components'>
+              <Tree.Branch label='Core'>
                 <Tree.Branch label='Tree Branch'>
                   <Tree.Item label='Tree Item' href='#' />
                   <Tree.Item label='Tree Item' href='#' />
                   <Tree.Item label='Tree Item' href='#' />
                 </Tree.Branch>
 
-                <Tree.Item label='Tree Item' href='#' />
-                <Tree.Item label='Tree Item' href='#' />
-                <Tree.Item label='Tree Item' href='#' />
-                <Tree.Item label='Tree Item' href='#' />
-                <Tree.Item label='Tree Item' href='#' />
-                <Tree.Item label='Tree Item' href='#' />
-                <Tree.Item label='Tree Item' href='#' />
+                <Tree.Item label='Accordion' href='#' />
+                <Tree.Item label='Button' href='#' />
+                <Tree.Item label='Checkbox' href='#' />
+                <Tree.Item label='Context Menu' href='#' />
+                <Tree.Item label='Dropdown' href='#' />
+                <Tree.Item label='Error Message' href='#' />
+                <Tree.Item label='Field' href='#' />
               </Tree.Branch>
             </Tree.Branch>
           </Tree>
