825b0b429eab58757c23978a88ac2093002a6ded
Karan <k.norastehnia@gmail.com>
2025-10-26 19:08:22 -0400

    Minor style updates

--- src/components/icons/Ellipsis/Ellipsis.tsx
+++ src/components/icons/Ellipsis/Ellipsis.tsx
@@ -23,8 +23,6 @@ const Ellipsis: React.FC<EllipsisProps> = ({
         ${styles['svg']} 
         ${styles[`color-${color}`]}
       `}
-      width='8'
-      height='2'
       viewBox='0 0 8 2'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
