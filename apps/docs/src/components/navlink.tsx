9507e755fb7fbf157c9723c38f4598049bd18e3d
Karan <k.norastehnia@gmail.com>
2025-11-15 20:23:19 -0500

    Update integration of Button with Next's Link component

--- apps/docs/src/components/navlink.tsx
+++ apps/docs/src/components/navlink.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { Button, type ButtonProps } from '@kaynora/ui'
-import { useRef } from 'react'
 
 interface NavLinkProps extends ButtonProps {
   action?: never
@@ -12,41 +11,9 @@ const NavLink: React.FC<NavLinkProps> = ({
   href,
   surface='text',
 }) => {
-  const nextLinkRef = useRef<HTMLAnchorElement>(null)
-  const buttonRef = useRef<HTMLElement>(null)
-
-  const passFocus = (e: React.FocusEvent) => {
-    if (e.target === nextLinkRef.current) {
-      buttonRef.current?.focus()
-      nextLinkRef.current.tabIndex = -1
-    }
-  }
-
-  const resetTabIndex = (e: React.FocusEvent) => {
-    if (!nextLinkRef.current) return
-
-    if (!e.currentTarget.contains(e.relatedTarget)) {
-      nextLinkRef.current.tabIndex = 0
-    }
-  }
-
   return (
-    <Link
-      href={href}
-      onFocus={passFocus}
-      onBlur={resetTabIndex}
-      ref={nextLinkRef}
-    >
-      <Button
-        surface={surface}
-        internal={{
-          root: {
-            tabIndex: -1,
-            ref: buttonRef,
-            "aria-hidden": "true",
-          }
-        }}
-      >
+    <Link href={href} tabIndex={-1}>
+      <Button surface={surface}>
         {children}
       </Button>
     </Link>
